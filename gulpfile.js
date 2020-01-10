a712ed294e4e7a0b2c5ac8ab553dad3d5485e8c0
Damián Silvani <munshkr@gmail.com>
2020-01-10 12:48:45 -0300

    Fix i18next gulp task and add update-locales script

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ var gulp = require('gulp');
 var scanner = require('i18next-scanner');
 
 gulp.task('i18next', function() {
-	return gulp.src(['src/**/*.{js,html}'])
+	return gulp.src(['*.{js,html}'])
 		.pipe(scanner({
-			lngs: ['en', 'de'], // supported languages
+			lngs: ['en', 'es', 'pt'], // supported languages
 			resource: {
 				// the source path is relative to current working directory
-				loadPath: 'assets/i18n/{{lng}}/{{ns}}.json',
+				loadPath: 'locales/{{lng}}/{{ns}}.json',
 
 				// the destination path is relative to your `gulp.dest()` path
-				savePath: 'i18n/{{lng}}/{{ns}}.json'
+				savePath: 'locales/{{lng}}/{{ns}}.json'
 			}
 		}))
-		.pipe(gulp.dest('assets'));
+		.pipe(gulp.dest('./'));
 });
